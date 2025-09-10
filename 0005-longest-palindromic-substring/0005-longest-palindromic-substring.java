424a2b3fec07e71fd7fdf7b101c2f31c5ee11994
Coder-Kathar <katharbasha1407@gmail.com>
2025-09-10 14:00:25 +0530

    Time: 16 ms (61.69%), Space: 45.9 MB (14.86%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,34 +1,26 @@
 class Solution {
+    private String check(String s, String ans, int l, int r)
+    {
+        while(s.charAt(l) == s.charAt(r))
+        {
+            l--;
+            r++;
+            if(l == -1 || r == s.length())
+                break;
+        }
+        String pali = s.substring(l+1,r);
+        if(pali.length() > ans.length())
+            ans = pali;
+        return ans;
+    }
     public String longestPalindrome(String s) {
         if(s.length() <= 1)
             return s;
         String ans = "";
         for(int i=1;i<s.length();i++)
         {
-            int l = i;
-            int r = i;
-            while(s.charAt(l) == s.charAt(r))
-            {
-                l--;
-                r++;
-                if(l == -1 || r == s.length())
-                    break;
-            }
-            String pali = s.substring(l+1,r);
-            if(pali.length() > ans.length())
-                ans = pali;
-            l = i-1;
-            r = i;
-            while(s.charAt(l) == s.charAt(r))
-            {
-                l--;
-                r++;
-                if(l == -1 || r == s.length())
-                    break;
-            }
-            pali = s.substring(l+1,r);
-            if(pali.length() > ans.length())
-                ans = pali;
+            ans = check(s,ans,i,i);
+            ans = check(s,ans,i-1,i);
         }
         return ans;
     }
