9d90dde2f4721c3905b2d335a78fc7f480a7792c
Coder-Kathar <katharbasha1407@gmail.com>
2025-09-27 22:40:21 +0530

    Time: 0 ms (100%), Space: 41.8 MB (35.4%) - LeetHub

---
+++ 2243-check-if-all-as-appears-before-all-bs/2243-check-if-all-as-appears-before-all-bs.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean checkString(String s) {
+        String str = "ba";
+        if(s.contains(str))
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+}
