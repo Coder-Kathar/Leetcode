cd87fc6ef1f5da96ccdc9a1427c7f23089a437a4
Coder-Kathar <katharbasha1407@gmail.com>
2026-02-16 11:20:12 +0530

    Time: 1 ms (99.56%), Space: 47.3 MB (56.02%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -4,13 +4,13 @@ class Solution {
         int l = 0, r = 0;
         while(r < nums.length-1)
         {
-            int farthest = 0;
-            for(int i=l;i<=r;i++)
+            int farthest = 0; // To find the larget distance cover at one jump
+            for(int i=l;i<=r;i++) // that's why ranging from L -> R
             {
                 farthest = Math.max(farthest, i+nums[i]);
             }
-            l = r + 1;
-            r = farthest;
+            l = r + 1; // update the next interval first element
+            r = farthest; // update the interval last element
             jumps++;
         }
         return jumps;
