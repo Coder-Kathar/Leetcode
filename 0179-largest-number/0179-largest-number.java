c3f304c90c596a2376f24f3c7f60451a96d05b32
Coder-Kathar <katharbasha1407@gmail.com>
2026-02-23 14:21:22 +0530

    Time: 6 ms (96.03%), Space: 45.2 MB (43.52%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -6,18 +6,7 @@ class Solution {
             numbers[i] = String.valueOf(nums[i]);
         }
         int n = numbers.length;
-        for(int i=0;i<n-1;i++)
-        {
-            for(int j=0;j<n-i-1;j++)
-            {
-                if((numbers[j] + numbers[j+1]).compareTo(numbers[j+1] + numbers[j]) < 0)
-                {
-                    String temp = numbers[j];
-                    numbers[j] = numbers[j+1];
-                    numbers[j+1] = temp;
-                }
-            }
-        }
+        Arrays.sort(numbers, (a,b) -> (b+a).compareTo(a+b));
         if(numbers[0].equals("0"))
             return "0";
         StringBuilder ans = new StringBuilder();
