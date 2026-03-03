72a459a9ab2bd86a9955e93dcc0bc0a67d724095
Coder-Kathar <katharbasha1407@gmail.com>
2026-03-03 14:25:44 +0530

    Time: 0 ms (100%), Space: 42.2 MB (47.03%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m+1][n+1];
+        return count(m,n,dp);
+    }
+    private int count(int r, int c, int[][] dp)
+    {
+        if(r == 1 || c == 1)
+            return 1;
+        if(dp[r][c] != 0)
+            return dp[r][c];
+        dp[r][c] = count(r-1,c,dp) + count(r,c-1,dp);
+        return dp[r][c];
+    }
+}
