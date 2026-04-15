b5e3d952a8e4d6b8a6da8160b20bc8cd7e6919ee
Coder-Kathar <katharbasha1407@gmail.com>
2026-04-15 14:17:27 +0530

    Time: 0 ms (100%), Space: 42.4 MB (94.5%) - LeetHub

--- 2103-rings-and-rods/2103-rings-and-rods.java
+++ 2103-rings-and-rods/2103-rings-and-rods.java
@@ -1,28 +1,32 @@
 class Solution {
     public int countPoints(String rings) {
-        StringBuilder[] sb = new StringBuilder[10];
-        for (int i = 0; i < 10; i++)
-        {
-            sb[i] = new StringBuilder();
-        }
+        boolean[][] freq = new boolean[10][3];
         for(int i=0;i<rings.length();i+=2)
         {
-            int ind = (rings.charAt(i+1))-'0';
-            sb[ind].append(rings.charAt(i));
+            int ind = (rings.charAt(i+1)) - '0';
+            char c = rings.charAt(i);
+            switch(c)
+            {
+                case 'R':
+                    freq[ind][0] = true;
+                    break;
+                case 'G':
+                    freq[ind][1] = true;
+                    break;
+                default:
+                    freq[ind][2] = true;
+            }
         }
         int count = 0;
-        for(StringBuilder x : sb)
+        for(boolean[] x : freq)
         {
-            if(x.length() > 2 && unique(x) == 3)
+            if(isAllTrue(x))
                 count++;
         }
         return count;
     }
-    private static int unique(StringBuilder x)
-    {
-        Set<Character> s = new HashSet<>();
-        for(int i=0;i<x.length();i++)
-            s.add(x.charAt(i));
-        return s.size();
-    }
+    private static boolean isAllTrue(boolean[] x)
+        {
+            return x[0] && x[1] && x[2];
+        }
 }
